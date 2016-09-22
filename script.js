36c50e0a8b641c6ad018e9582c550f8a4a9bd305
Guillaume <fan.fan@live.fr>
2016-09-22 16:10:18 +0200

    Update Accueil

--- script.js
+++ script.js
@@ -8,4 +8,32 @@ $(document).ready(function() {
             $('#push').removeClass('navbar-fixed');
         }
     });
+
+    $('.carousel.carousel-slider').carousel({
+        full_width: true
+    });
+    var first = true;
+    autoplay1();
+
+    function autoplay1() {
+        if (first == true) {
+            first = false;
+        } else {
+            $('.carousel').carousel('next');
+        }
+        setTimeout(autoplay1, 6000);
+    }
+
+    $('#slider2').carousel();
+    var second = true;
+    autoplay2();
+
+    function autoplay2() {
+        if (second == true) {
+            second = false;
+        } else {
+            $('#slider2').carousel('next');
+        }
+        setTimeout(autoplay2, 3000);
+    }
 });
